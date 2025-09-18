64eb199111c92b30b5fd49d74c6708acaa267989
Jitendra Chaudhary <jitendra72003@gmail.com>
2025-09-18 22:25:44 +0530

    Create 268_Missing_Number.cpp

---
+++ Array/Easy/268_Missing_Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int result = n;
+        int l = 0, r = n-1;
+        while(l<=r) {
+            int mid = l + (r - l)/2;
+            if(nums[mid] > mid) {
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return result;
+    }
+};
