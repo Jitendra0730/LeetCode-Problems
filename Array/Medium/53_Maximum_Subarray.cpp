07186b21d4d13eb9d41cf0c5cc288f2e8d246186
Jitendra Chaudhary <jitendra72003@gmail.com>
2025-10-01 20:31:46 +0530

    Create 53_Maximum_Subarray.cpp

---
+++ Array/Medium/53_Maximum_Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSoFar = nums[0];
+        int currMax = nums[0];
+        //Kadane's Algorithm 
+        for(int i = 1; i < nums.size(); i++) {
+            currMax = max(nums[i], nums[i] + currMax);
+            maxSoFar = max(currMax, maxSoFar);
+        }
+        return maxSoFar;
+    }
+};
