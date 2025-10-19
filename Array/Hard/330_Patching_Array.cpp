3e9d8f9631fd8a922e77718c31f9579b40e3001e
Jitendra Chaudhary <jitendra72003@gmail.com>
2025-10-19 11:24:54 +0530

    Create 330_Patching_Array.cpp

---
+++ Array/Hard/330_Patching_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, long n) {
+        long miss = 1;
+        int patches = 0;
+        int i = 0;
+
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+};
