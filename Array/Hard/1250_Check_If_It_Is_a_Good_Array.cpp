57472ac1871ae6074c3fbcfcdc00014a44fc41f8
Jitendra Chaudhary <jitendra72003@gmail.com>
2025-10-30 21:25:53 +0530

    Create 1250_Check_If_It_Is_a_Good_Array.cpp

---
+++ Array/Hard/1250_Check_If_It_Is_a_Good_Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int g = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            g = gcd(g, nums[i]);
+            if (g == 1) return true; // early stop
+        }
+        return g == 1;
+    }
+};
